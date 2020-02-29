519f04a35394cc53facfabfaeaf09e5ab03fe89d
Jayvee Ramos <Jayvee@ag-investment.net>
2020-02-29 17:57:38 +0800

    -modules

--- control/web/src/main/java/com/example/control/web/WebApplication.java
+++ control/web/src/main/java/com/example/control/web/WebApplication.java
@@ -1,11 +1,10 @@
-package com.example.web;
+package com.example.control.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(WebApplication.class, args);
   }
